f3a69b9efe03a71f51004837ce82426bcf69ba31
Danica0204 <balidio_danica@fbc.ph.education>
2024-12-20 23:51:05 +0800

    port

---
+++ script.js
@@ -0,0 +1,12 @@
+ScrollReveal({ 
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.home', { origin: 'top' });
+ScrollReveal().reveal('.skills', { origin: 'left' });
+ScrollReveal().reveal('.info', { origin: 'right' });
+
+
